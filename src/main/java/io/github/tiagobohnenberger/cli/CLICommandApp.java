2fa986064b7b051f45193b458b203120346a8ba3
Tiago Bohnenberger <tiagoboh10@gmail.com>
2025-01-08 23:48:55 -0300

    Renames main class

--- src/main/java/io/github/tiagobohnenberger/cli/CLICommandApp.java
+++ src/main/java/io/github/tiagobohnenberger/cli/CLICommandApp.java
@@ -2,7 +2,7 @@ package io.github.tiagobohnenberger.cli;
 
 import org.jboss.weld.environment.se.StartMain;
 
-public class AdopetConsoleApplication {
+public class CLICommandApp {
 
     public static void main(String[] args) {
         StartMain.main(args);
